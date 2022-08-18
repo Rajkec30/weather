d21c3ee1554ef7789f8db7155eab53764e68b2ab
raian <raianmelon11@gmail.com>
2022-08-18 13:13:14 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,72 @@
+<script>
+    import {onMount} from 'svelte'
+
+    let loaded = false
+    let vrijeme;
+
+    let place = ''
+    let lat = ''
+    let lon = ''
+
+    const getWeather = ( () => {
+        navigator.geolocation.getCurrentPosition(async (position) => {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=cb848c3a3dd3ef2de686991afb87103e`)
+            vrijeme = await response.json()
+            console.log(vrijeme)
+            loaded = true
+        })
+    })
+
+    const find_search = (async (place) => {
+        if (place){
+            const response = await fetch(`https://api.locationiq.com/v1/autocomplete?key=pk.238e940315639f0eb8622276b1f1e9c5&q=${place}`)
+            let vrijeme2 = await response.json()
+            lat = vrijeme2[0].lat
+            lon = vrijeme2[0].lon
+            const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=cb848c3a3dd3ef2de686991afb87103e`)
+            vrijeme = await res.json()
+            console.log(vrijeme)
+            loaded = true
+        } else {
+            console.log("Wrong place")
+        }
+    })
+
+    const handleSubmit = () => {
+        find_search(place)
+        place = ''
+    }
+
+    onMount( () => {
+        getWeather()
+    });
+
+    let now = new Date();
+    $: timestamp = now.getDate() + '.' + (now.getMonth()+1) + '.' +now.getFullYear() + '.'
+</script>
+
+<main class="bg-[url('./assets/104824.webp')]">
+    <div class="font-['poppins text-[#0070ef] h-screen flex flex-col justify-center items-center">
+        {#if !loaded}
+            <div class="fixed top-0 left-0 right-0 bottom-0 grid place-items-center z-50">
+                <div class="bg-[url('./assets/Rolling-1s-200px.svg')] w-40 h-40 img bg-no-repeat"></div>
+            </div>
+        {/if}
+        <slot />
+        {#if vrijeme}
+            <div class="flex flex-col justify-center items-start drop-shadow-2xl rounded-3xl bg-white p-14 w-6/12 text-3xl gap-8">
+                <form class="text-gray-800 " on:submit|preventDefault={handleSubmit}>
+                    <input class="outline-0 focus:ring-2 focus:rounded p-1 border-b-2 focus:border-0" type="text" bind:value={place} placeholder="Grad">
+                    <button class="bg-[#0070ef] text-white px-2 py-1 rounded">Pretrazi</button>
+                </form>
+                <div class="flex gap-7 text-4xl">
+                    <h1>{vrijeme.name}</h1>
+                    <h1 class="text-yellow-400">{Math.round(vrijeme.main.temp) /10}C</h1>
+                </div>
+                <h1 class="">{timestamp}</h1>
+                <h1>Oblaci: {vrijeme.weather[0].description}</h1>
+                <h1>Vjetar: {Math.round(vrijeme.wind.speed * 1.6)}km/h</h1>
+            </div>
+        {/if}
+    </div>
+</main>
